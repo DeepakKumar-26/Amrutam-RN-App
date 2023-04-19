d4b059d28d71dd2eb7cfb916ceb84dcca256fa7a
Deepak Kumar <deepakkum261626@gmail.com>
2023-04-19 18:51:13 +0530

    added blog data

--- app/data/blogs.js
+++ app/data/blogs.js
@@ -5,6 +5,8 @@ export const blogs = [
     description:
       'Ayurveda entails practices that honor the body, mind and soul! In this article, we share with you five therapeutic ayurvedic practices to heal and nourish your body.Shiro Abhyanga or Head Massage Comprised of two words Shiro (head) and Abhyanga (massage), it essentially means a complete massage of the head, neck, and shoulders using Ayurvedic herbal oils These areas are most affected by stress, which is why Shiro Abhyanga is beneficial in reducing pain and stress in these regions',
     posted_at: '06 Dec 2022',
+    posted_by: 'Team Amrutam',
+    category: 'top-posts',
     thumbnail_url:
       'https://cdn.shopify.com/s/files/1/0645/9650/8925/files/5x_600x600.jpg',
   },
@@ -15,6 +17,8 @@ export const blogs = [
     description:
       'I lived in Gwalior with my parents for five years. I spent all these years in a disciplined environment. Half a decade later, when I moved out to Bangalore from my home town, so much changed in the modern lifestyle.',
     posted_at: '07 Oct 2022',
+    posted_by: 'Team Amrutam',
+    category: 'top-posts',
     thumbnail_url:
       'https://cdn.shopify.com/s/files/1/0645/9650/8925/articles/yuris-alhumaydy-mSXMHkgRs8s-unsplash.jpg',
   },
@@ -24,7 +28,141 @@ export const blogs = [
     description:
       'Our eyes are being used more than ever, especially with the increased use of mobile and laptop screens in the last few decades.',
     posted_at: '19 Sep 2022',
+    posted_by: 'Team Amrutam',
+    category: 'top-posts',
     thumbnail_url:
       'https://cdn.shopify.com/s/files/1/0645/9650/8925/articles/amanda-dalbjorn-UbJMy92p8wk-unsplash.jpg',
+  },
+  {
+    id: 'b4',
+    title:
+      'There is a Buddha in everybody; we need to tap into that part of us": Shark Tank featured founder Ridhima Arora',
+    description:
+      'It took a pandemic for many people to get closer to their Ayurvedic roots.',
+    posted_at: '19 Sep 2022',
+    posted_by: 'Team Amrutam',
+    category: 'top-posts',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/files/Namhya-Foods-on-Shark-Tank-India_600x600.jpg',
+  },
+  {
+    id: 'b5',
+    title:
+      'There is a Buddha in everybody; we need to tap into that part of us": Shark Tank featured founder Ridhima Arora',
+    description:
+      'It took a pandemic for many people to get closer to their Ayurvedic roots.',
+    posted_at: '19 Sep 2022',
+    posted_by: 'Team Amrutam',
+    category: 'rediscovering-ayurveda',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/files/Namhya-Foods-on-Shark-Tank-India_600x600.jpg',
+  },
+  {
+    id: 'b6',
+    title: 'Eyes and Ayurveda',
+    description:
+      'Our eyes are being used more than ever, especially with the increased use of mobile and laptop screens in the last few decades.',
+    posted_at: '19 Sep 2022',
+    posted_by: 'Team Amrutam',
+    category: 'rediscovering-ayurveda',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/articles/amanda-dalbjorn-UbJMy92p8wk-unsplash.jpg',
+  },
+  {
+    id: 'b7',
+    title: '5 Therapeutic Ayurvedic Practices to Nourish Your Body',
+    description:
+      'Ayurveda entails practices that honor the body, mind and soul! In this article, we share with you five therapeutic ayurvedic practices to heal and nourish your body.Shiro Abhyanga or Head Massage Comprised of two words Shiro (head) and Abhyanga (massage), it essentially means a complete massage of the head, neck, and shoulders using Ayurvedic herbal oils These areas are most affected by stress, which is why Shiro Abhyanga is beneficial in reducing pain and stress in these regions',
+    posted_at: '06 Dec 2022',
+    posted_by: 'Team Amrutam',
+    category: 'rediscovering-ayurveda',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/files/5x_600x600.jpg',
+  },
+  {
+    id: 'b8',
+    title:
+      'Reflecting on my Modern lifestyle - is this the way to live? Can Ayurveda help us live better?',
+    description:
+      'I lived in Gwalior with my parents for five years. I spent all these years in a disciplined environment. Half a decade later, when I moved out to Bangalore from my home town, so much changed in the modern lifestyle.',
+    posted_at: '07 Oct 2022',
+    posted_by: 'Team Amrutam',
+    category: 'rediscovering-ayurveda',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/articles/yuris-alhumaydy-mSXMHkgRs8s-unsplash.jpg',
+  },
+];
+
+export const postCategories = [
+  {
+    id: 'pc1',
+    category: 'top-posts',
+    title: 'Top Posts',
+  },
+  {
+    id: 'pc2',
+    category: 'all-blogs',
+    title: 'All Blogs',
+  },
+  {
+    id: 'pc3',
+    category: 'rediscovering-ayurveda',
+    title: 'Rediscovering Ayurveda',
+  },
+  {
+    id: 'pc4',
+    category: 'amrutam-nari',
+    title: 'Amrutam Nari',
+  },
+  {
+    id: 'pc5',
+    category: 'amrutam-contributors',
+    title: 'Amrutam Contributors',
+  },
+];
+export const featuredPosts = [
+  {
+    id: 'fp1',
+    title: 'Our Honest Journey of Pitching in Shark Tank India Season 2',
+    description:
+      "Our journey from building Amrutam for six years to appearing on national TV in front of 30+ million people. What a roller coaster! Here is my honest attempt at sharing my experience of taking Amrutam to Shark Tank India. The backstory Usually, words flow out of me, but this one is going to be tough, I am aware. In 2017, when D2C wasn't a thing, and online business was still not taken seriously [in at least tier-2,3 cities], we pledged to go online and sell products only on our website. We wanted to build a community of people. Hold on, a couple of years before that -  2015, when I was in the second year of my Master's degree in Clinical Psychology at TISS and working day and night on my application for my dream university - the University of Edinburgh, I didn't know I would be writing this post and talking about featuring my family’s business of making authentic ayurvedic recipes in Shark Tank. Shark Tank India wasn't something we speculated about eight years ago. ",
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/files/Shark-tank-Amrutam-photo3_600x600.jpg',
+    posted_by: 'Team Amrutam',
+    posted_at: '11 Dec 2021',
+    category: 'top-posts',
+  },
+  {
+    id: 'fp2',
+    title: 'Rediscovering Ayurveda X Ankita Lodha',
+    description:
+      "In this week's article on our Rediscovering Ayurveda series, we interviewed Ankita Lodha",
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/articles/lisa-hobbs-mRaNok_Ld6s-unsplash.jpg',
+    posted_by: 'Team Amrutam',
+    posted_at: '11 Dec 2021',
+    category: 'top-posts',
+  },
+  {
+    id: 'fp3',
+    title: 'Rediscovering Ayurveda with Muskan Soni',
+    description:
+      'Well, you all will know a typical Indian medical aspirant, wanting to chase the MBBS? All the high hopes and lots of struggle. I didn’t make it.',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/files/IMG_7924-min-1024x683_480x480.jpg',
+    posted_by: 'Team Amrutam',
+    posted_at: '11 Dec 2021',
+    category: 'top-posts',
+  },
+  {
+    id: 'fp4',
+    title: 'Rediscovering Ayurveda X Soumya Pathak',
+    description:
+      'Rediscovering Ayurveda is Amrutam`s series of blogs which will include various men and women and their ideas about Ayurveda.',
+    thumbnail_url:
+      'https://cdn.shopify.com/s/files/1/0645/9650/8925/articles/1_2.jpg',
+    posted_by: 'Team Amrutam',
+    posted_at: '11 Dec 2021',
+    category: 'top-posts',
   },
 ];
